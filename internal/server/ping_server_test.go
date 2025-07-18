13342610542c6331e0ab33b7ee829980f3f1aa33
Rusticov <knightdestruction@pm.me>
2025-07-18 08:33:40 +0100

    Extend `PingServer` tests: increase timeout and add test for PING with message.
    Working running against live Redis server for test validation.

--- internal/server/ping_server_test.go
+++ internal/server/ping_server_test.go
@@ -11,7 +11,7 @@ import (
 
 func TestPingServer(t *testing.T) {
 
-	timeout := 10 * time.Millisecond
+	timeout := 100 * time.Millisecond
 
 	t.Run("send ping without message and receive PONG", func(t *testing.T) {
 		testServer := createTestServer(t)
@@ -30,6 +30,25 @@ func TestPingServer(t *testing.T) {
 
 		response := string(buffer[:n])
 		assert.Equal(t, "+PONG\r\n", response)
+	})
+
+	t.Run("send ping with message should receive message back in reply", func(t *testing.T) {
+		testServer := createTestServer(t, RealRedisServer)
+		defer testServer.Close()
+
+		connection, err := net.DialTimeout("tcp", testServer.Address(), timeout)
+		require.NoError(t, err)
+		defer connection.Close()
+
+		_, err = connection.Write([]byte("*2\r\n$4\r\nPING\r\n$11\r\nthe message\r\n"))
+		require.NoError(t, err)
+
+		buffer := make([]byte, 256)
+		n, err := connection.Read(buffer)
+		assert.NoError(t, err)
+
+		response := string(buffer[:n])
+		assert.Equal(t, "$11\r\nthe message\r\n", response)
 	})
 }
 
