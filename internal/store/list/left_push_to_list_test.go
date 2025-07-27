ec48b07ee738ad05366a2732646f46e3bb7a7d32
Rusticov <knightdestruction@pm.me>
2025-07-27 09:54:41 +0100

    Replace `require` assertions with `assert` in list push tests.

--- internal/store/list/left_push_to_list_test.go
+++ internal/store/list/left_push_to_list_test.go
@@ -1,7 +1,7 @@
 package list_test
 
 import (
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 	"redis-challenge/internal/store/list"
 	"testing"
 )
@@ -11,27 +11,27 @@ func TestAddValuesToList(t *testing.T) {
 	t.Run("left push string to empty list", func(t *testing.T) {
 		newList, errorData := list.LeftPushToOldList([]string{"a"}, nil)
 
-		require.Nil(t, errorData, "should not return an error")
-		require.Equal(t, []string{"a"}, newList)
+		assert.Nil(t, errorData, "should not return an error")
+		assert.Equal(t, []string{"a"}, newList)
 	})
 
 	t.Run("left push 2 strings to empty list should reverse order", func(t *testing.T) {
 		newList, errorData := list.LeftPushToOldList([]string{"a", "b"}, nil)
 
-		require.Nil(t, errorData, "should not return an error")
-		require.Equal(t, []string{"b", "a"}, newList)
+		assert.Nil(t, errorData, "should not return an error")
+		assert.Equal(t, []string{"b", "a"}, newList)
 	})
 
 	t.Run("left push string to non-empty list of strings", func(t *testing.T) {
 		newList, errorData := list.LeftPushToOldList([]string{"a", "b", "c"}, []string{"d", "e", "f"})
 
-		require.Nil(t, errorData, "should not return an error")
-		require.Equal(t, []string{"c", "b", "a", "d", "e", "f"}, newList, "only new values are reversed")
+		assert.Nil(t, errorData, "should not return an error")
+		assert.Equal(t, []string{"c", "b", "a", "d", "e", "f"}, newList, "only new values are reversed")
 	})
 
 	t.Run("left push string to old value that is not a list should error", func(t *testing.T) {
 		_, errorData := list.LeftPushToOldList([]string{"a", "b", "c"}, "f")
 
-		require.Equal(t, list.ErrorOldValueIsNotList, errorData, "should return an error")
+		assert.Equal(t, list.ErrorOldValueIsNotList, errorData, "should return an error")
 	})
 }
--- internal/store/list/right_push_to_list_test.go
+++ internal/store/list/right_push_to_list_test.go
@@ -1,7 +1,7 @@
 package list_test
 
 import (
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 	"redis-challenge/internal/store/list"
 	"testing"
 )
@@ -11,27 +11,27 @@ func TestRightPushValuesToList(t *testing.T) {
 	t.Run("right push string to empty list", func(t *testing.T) {
 		newList, errorData := list.RightPushToOldList([]string{"a"}, nil)
 
-		require.Nil(t, errorData, "should not return an error")
-		require.Equal(t, []string{"a"}, newList)
+		assert.Nil(t, errorData, "should not return an error")
+		assert.Equal(t, []string{"a"}, newList)
 	})
 
 	t.Run("right push 2 strings to empty list should be in order", func(t *testing.T) {
 		newList, errorData := list.RightPushToOldList([]string{"a", "b"}, nil)
 
-		require.Nil(t, errorData, "should not return an error")
-		require.Equal(t, []string{"a", "b"}, newList)
+		assert.Nil(t, errorData, "should not return an error")
+		assert.Equal(t, []string{"a", "b"}, newList)
 	})
 
 	t.Run("right push string to non-empty list of strings", func(t *testing.T) {
 		newList, errorData := list.RightPushToOldList([]string{"a", "b", "c"}, []string{"d", "e", "f"})
 
-		require.Nil(t, errorData, "should not return an error")
-		require.Equal(t, []string{"d", "e", "f", "a", "b", "c"}, newList, "value are in order")
+		assert.Nil(t, errorData, "should not return an error")
+		assert.Equal(t, []string{"d", "e", "f", "a", "b", "c"}, newList, "value are in order")
 	})
 
 	t.Run("right push string to old value that is not a list should error", func(t *testing.T) {
 		_, errorData := list.RightPushToOldList([]string{"a", "b", "c"}, "f")
 
-		require.Equal(t, list.ErrorOldValueIsNotList, errorData, "should return an error")
+		assert.Equal(t, list.ErrorOldValueIsNotList, errorData, "should return an error")
 	})
 }
