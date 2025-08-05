683372d247906e5f5adf84e915809c8fc696e86e
Rusticov <knightdestruction@pm.me>
2025-08-05 08:35:29 +0100

    Move all DoubleEndedList code into `double_ended_list.go` for better cohesion and readability.

--- internal/list/double_ended_list.go
+++ internal/list/double_ended_list.go
@@ -1,5 +1,14 @@
 package list
 
+type DoubleEndedList struct {
+	left  []string
+	right []string
+}
+
+func (l DoubleEndedList) Length() int {
+	return len(l.left) + len(l.right)
+}
+
 func LeftPush(newValues []string, storedData any) (DoubleEndedList, bool) {
 	list, ok := parseListFromStoredData(storedData)
 	if !ok {
@@ -18,4 +27,14 @@ func RightPush(newValues []string, storedData any) (DoubleEndedList, bool) {
 
 	list.right = append(list.right, newValues...)
 	return list, true
+}
+
+func parseListFromStoredData(storedData any) (DoubleEndedList, bool) {
+	if storedData == nil {
+		return DoubleEndedList{}, true
+	}
+	if stringList, ok := storedData.(DoubleEndedList); ok {
+		return stringList, true
+	}
+	return DoubleEndedList{}, false
 }
